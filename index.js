91acc361e295729646236a11a4b2e584d97722c0
Mikey <mgevarts@outlook.com>
2020-07-08 10:43:43 -0500

    agree

---
+++ index.js
@@ -0,0 +1,126 @@
+const REPL = false;
+
+var cache = [];
+const uncolor = require("uncolor");
+const Readline = require("readline");
+var tosrv = false;
+const chunk2String = (chk) => {
+    return uncolor(chk.toString()).trim();
+};
+let rl = Readline.createInterface({
+    input: process.stdin
+});
+rl.on("line", (raw) => {
+    let chunk = chunk2String(raw);
+    if (!tosrv) cache.push(chunk);
+    else tosrv(chunk);
+});
+(async () => {
+    if (!REPL) {
+        require("dotenv").config();
+    }
+    var mcexec = require("./lib/mcexec");
+    const tunnel = require("./lib/tunnel");
+    const pathRes = require("path").resolve;
+    const Express = require("express");
+    const basicAuth = require("express-basic-auth");
+    const cors = require("cors");
+    const app = Express();
+    var users = {};
+    var conn = false;
+    users[process.env.SERVER_DOT] = process.env.SERVER_TOKEN;
+    app.use(basicAuth({
+        users: users,
+        challenge: true
+    }));
+    app.use(cors());
+    app.use(Express.static("./static"));
+    require("express-ws")(app);
+
+    app.get("/", (req, res) => {
+        console.log("GET /");
+        res.status(200).sendFile(pathRes("./index.html"));
+    });
+    app.ws("/", (ws, req) => {
+        ws.active = false;
+        console.log("WS /");
+        ws.sendJSON = (type, data) => {
+            return ws.send(JSON.stringify({
+                "type": type,
+                "data": data
+            }));
+        };
+        ws.fromNode = (data) => {
+            return ws.sendJSON("message", {
+                "from": "node",
+                "data": data
+            });
+        };
+        ws.fromMC = (data) => {
+            return ws.sendJSON("message", {
+                "from": "mc",
+                "data": data
+            });
+        };
+        ws.onmessage = async (e) => {
+            if (!ws.active) {
+                if (conn) {
+                    ws.sendJSON("conn");
+                } else {
+                    conn = true;
+                    ws.active = true;
+                }
+            }
+            let msg = JSON.parse(e.data);
+            switch (msg.type) {
+                case "ready": {
+                    console.log("WS ready");
+                    ws.sendJSON("cache", cache);
+                    var actions = [];
+                    const tunwstat = async (chunk) => {
+                        if (chunk.includes("INFO]: Starting Minecraft server on *:1025")) {
+                            ws.fromNode("Opening tunnel...");
+                            let urls = await tunnel();
+                            ws.fromNode(`Tunnel opened to ${urls[1]}! (Raw URL for debug purposes: ${urls[0]})`);
+                            return true;
+                        } else return false;
+                    };
+                    const rconwstat = async (chunk) => {
+                        if (chunk.includes("INFO]: RCON running on 0.0.0.0:1026")) {
+                            ws.fromNode("Connecting to RCON [allows you to execute console commands]...");
+                            mcexec = await mcexec();
+                            ws.sendJSON("rcon");
+                            ws.fromNode("RCON connected! You can now send commands using the text box below.");
+                            return true;
+                        } else return false;
+                    };
+                    if ((await tunwstat(cache.join("\n"))) == false) actions.push(tunwstat);
+                    if ((await rconwstat(cache.join("\n"))) == false) actions.push(rconwstat);
+                    tosrv = async (chunk) => {
+                        let nulls = 0;
+                        for (let j in actions) {
+                            let i = actions[j];
+                            if (!i || i == null) nulls++; else if (await i(chunk)) delete actions[j];
+                        }
+                        ws.fromMC(chunk);
+                        if (nulls == actions.length) tosrv = ws.fromMC;
+                    };
+                    console.log(actions, tosrv);
+                    console.log("Cache flushed and listeners setup.");
+                    break;
+                }
+                case "cmd": {
+                    mcexec(msg.data);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        };
+    });
+
+    app.listen(1027, () => {
+        console.log("server listening");
+    });
+})();
---
+++ static/client.js
@@ -0,0 +1,64 @@
+const id = (eid) => {
+    return document.getElementById(eid);
+};
+window.onload = () => {
+    id("mbody").innerText = "Connecting...";
+    let url = new URL(window.location);
+    url.protocol = url.protocol.replace("http", "ws");
+    const ws = new WebSocket(url.href);
+    ws.sendJSON = (type, data) => {
+        return ws.send(JSON.stringify({
+            "type": type,
+            "data": data
+        }));
+    };
+    ws.onmessage = (e) => {
+        let msg = JSON.parse(e.data);
+        console.log(msg);
+        switch (msg.type) {
+            case "conn": {
+                document.body.innerText = "You're already connected to the console. Please return to that tab to keep using XPire Console.";
+                ws.close();
+                break;
+            }
+            case "message": {
+                let elem = document.createElement("div");
+                elem.className = msg.data.from;
+                elem.innerText = msg.data.data;
+                id("msgs").appendChild(elem);
+                break;
+            }
+            case "cache": {
+                for (let i of msg.data) {
+                    let elem = document.createElement("div");
+                    elem.className = "mc";
+                    elem.innerText = i;
+                    id("msgs").appendChild(elem);
+                }
+                break;
+            }
+            case "rcon": {
+                id("consolecmd").hidden = false;
+                id("mcinp").onkeyup = (e) => {
+                    if (e.key.toLowerCase() == "enter") {
+                        let cmd = id("mcinp").value;
+                        id("mcinp").value = "";
+                        let elem = document.createElement("div");
+                        elem.className = "cmd";
+                        elem.innerText = cmd;
+                        id("msgs").appendChild(elem);
+                        ws.sendJSON("cmd", cmd);
+                    }
+                };
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+    ws.onopen = () => {
+        id("mbody").innerHTML = `<div id="msgs">Messages [Green is directly from MC, orange is from Xpire Helper]:</div><div hidden id="consolecmd"><br /><label for="mcinp">Execute Minecraft Console Command: /</label><input id="mcinp" placeholder="Command" /></div>`;
+        ws.sendJSON("ready");
+    };
+};
