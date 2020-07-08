de8e89b5f976302a59d76c03fae2e337a7b3b849
Mikey <mgevarts@outlook.com>
2020-07-08 11:43:28 -0500

    agree

--- dl.sh
+++ dl.sh
@@ -2,8 +2,9 @@ echo By executing this script you agree to the JRE License, the PaperMC license,
 echo the Mikeylab License, the Mojang Minecraft EULA,
 echo the NPM license, the MIT license,
 echo and the licenses of all packages used \in this project.
-echo Press Ctrl+C NOW \if you \do not agree to any of these licenses.
-sleep 10 &&
+echo Press Ctrl+C \if you \do not agree to any of these licenses.
+echo Press Enter to agree.
+read -s agree_text &&
 echo Thank you \for agreeing, the download will now begin.
 wget -O jre.tar.gz "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=242050_3d5a2bb8f8d4428bbe94aed7ec7ae784" &&
 tar -zxf jre.tar.gz &&
